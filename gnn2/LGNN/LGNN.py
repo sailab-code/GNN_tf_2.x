176630c3027ddcde81d0b1016cad091572683305
NickDrake117 <63723473+NickDrake117@users.noreply.github.com>
2021-03-02 17:30:32 +0100

    Add files via upload

--- gnn2/LGNN/LGNN.py
+++ gnn2/LGNN/LGNN.py
@@ -24,11 +24,10 @@ class LGNN(BaseGNN):
                  extra_metrics_arguments: Optional[dict[str, dict]] = None,
                  path_writer: str = 'writer/',
                  namespace: str = 'LGNN') -> None:
-        ### CHECK LENGTHs
-        # if type(net_state_params) == dict: net_state_params = [net_state_params.copy() for _ in range(layers)]
-        # if type(net_output_params) == dict: net_output_params = [net_output_params.copy() for _ in range(layers)]
+
         # BaseGNN constructor
         super().__init__(optimizer, loss_function, loss_arguments, addressed_problem, extra_metrics, extra_metrics_arguments, path_writer, namespace)
+
         ### LGNNs parameter
         self.get_state = get_state
         self.get_output = get_output
@@ -69,44 +68,69 @@ class LGNN(BaseGNN):
     ## CALL/PREDICT METHOD ############################################################################################
     def __call__(self, g: GraphObject) -> tf.Tensor:
         """ return ONLY the LGNN output for graph g of type GraphObject """
-        return self.Loop(g, training=False)[-1]
+        out = tf.reduce_sum(self.Loop(g, training=False)[-1], axis=0)
+        return self.gnns[-1].output_activation(out)
+
+    # -----------------------------------------------------------------------------------------------------------------
+    def predict(self, g: GraphObject, idx:Union[int, list[int], range]=-1) -> Union[tf.Tensor, list[tf.Tensor]]:
+        if type(idx)==int: assert idx in range(-self.layers, self.layers)
+        elif isinstance(idx, (list, range)): assert all(i in range(-self.layers, self.layers) for i in idx) and list(idx)==sorted(idx)
+        else: raise ValueError('param <idx> must be int or list of int in range(-self.layers, self.layers)')
+
+        # get only outputs, without iteration and states
+        out = [gnn.output_activation(o) for gnn,o in zip(self.gnns, self.Loop(g, training=False)[-1])]
+        return out[idx] if type(idx)==int else [out[i] for i in idx]
 
 
-    ## EVALUATE METHODs ###############################################################################################
+    ## EVALUATE METHODS ###############################################################################################
+    def evaluate_single_graph(self, g: GraphObject, class_weights: Union[int, float, list[float]], training: bool) -> tuple:
+        it, loss_weight, targs, out = super().evaluate_single_graph(g, class_weights, training)
+        loss = tf.reduce_sum([self.loss_function(targs, i, **self.loss_args) * loss_weight for i in out], axis=0)
+        # Add a residual connection to the lgnn output, s.t. outputs are merged and then are processed by last output activation
+        out = tf.reduce_sum(out, axis=0)
+        return it, loss, targs, self.gnns[-1].output_activation(out)
+
+
+    ## LOOP METHODS ###################################################################################################
     def update_labels(self, g: GraphObject, state: Union[tf.Tensor, array], output: Union[tf.Tensor, array]) \
             -> tuple[Optional[tf.Tensor], Optional[tf.Tensor]]:
         arcplus, nodeplus = None, None
         # check state
         if self.get_state: nodeplus = state
+
         # check output
         if self.get_output:
-	        # process output to make it shape compatible
-	        mask = tf.logical_and(g.getSetMask(), g.getOutputMask())
-	        row = g.nodes.shape[0] if g.problem_based != 'a' else g.arcs.shape[0]
-	        out = zeros((row, g.targets.shape[1]))
-	        out[mask] = output
-        	if g.problem_based != 'a' and nodeplus is None: nodeplus = out
-        	elif g.problem_based !='a': nodeplus = tf.concat([nodeplus, out], axis=1)
-        	else: arcplus = out
+            # process output to make it shape compatible
+            mask = tf.logical_and(g.getSetMask(), g.getOutputMask())
+            row = g.nodes.shape[0] if g.problem_based != 'a' else g.arcs.shape[0]
+            out = zeros((row, g.targets.shape[1]))
+            out[mask] = output
+            if g.problem_based != 'a' and nodeplus is None: nodeplus = out
+            elif g.problem_based != 'a': nodeplus = tf.concat([nodeplus, out], axis=1)
+            else: arcplus = out
+
         return nodeplus, arcplus
 
-
-    ## LOOP METHODS ###################################################################################################
+    # -----------------------------------------------------------------------------------------------------------------
     def Loop(self, g: GraphObject, *, nodeplus=None, arcplus=None, training: bool = False) -> tuple[list[int], tf.Tensor, tf.Tensor]:
         # graph processing
         K, outs, nodeplus, arcplus = list(), list(), None, None
         for idx, gnn in enumerate(self.gnns[:-1]):
-            if type(gnn)==GNNgraphBased:
+
+            if type(gnn) == GNNgraphBased:
                 k, state, out = super(GNNgraphBased, gnn).Loop(g, nodeplus=nodeplus, arcplus=arcplus, training=training)
                 nodegraph = tf.constant(g.getNodeGraph(), dtype=tf.float32)
                 outs.append(tf.matmul(nodegraph, out, transpose_a=True))
+
             else:
                 k, state, out = gnn.Loop(g, nodeplus=nodeplus, arcplus=arcplus, training=training)
                 outs.append(out)
+
             K.append(k)
-            nodeplus, arcplus = self.update_labels(g, state, out)
+            nodeplus, arcplus = self.update_labels(g, state, gnn.output_activation(out))
+
         k, state, out = self.gnns[-1].Loop(g, nodeplus=nodeplus, arcplus=arcplus, training=training)
-        return K + [k], state, outs+[out] #tf.reduce_sum(outs + [out], axis=0)
+        return K + [k], state, outs + [out]
 
 
     ## TRAINING METHOD ################################################################################################
@@ -125,6 +149,7 @@ class LGNN(BaseGNN):
         :param verbose: (int) 0: silent mode; 1: print history; 2: print epochs/batches, 3: history + epochs/batches. Default 3.
         :return: None
         """
+
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         def checktype(elem: Optional[Union[GraphObject, list[GraphObject]]]) -> list[GraphObject]:
             """ check if type(elem) is correct. If so, return None or a list og GraphObjects """
@@ -142,24 +167,24 @@ class LGNN(BaseGNN):
             if arcplus is not None: g.arcs = concatenate([g.arcs, arcplus.numpy()], axis=1)
             return g
 
-
         ### TRAINING FUNCTION -----------------------------------------------------------------------------------------
         if serial_training:
             from numpy import concatenate
             gTr, gVa = checktype(gTr), checktype(gVa)
             gTr1, gVa1 = [i.copy() for i in gTr], [i.copy() for i in gVa] if gVa else None
-            
+
             for idx, gnn in enumerate(self.gnns):
-                if verbose in [1,3]: print('\n\n------------------- GNN{} -------------------\n'.format(idx))
-                
+                if verbose in [1, 3]: print('\n\n------------------- GNN{} -------------------\n'.format(idx))
+
                 # train the idx-th gnn
                 gnn.train(gTr1, epochs, gVa1, update_freq, max_fails, class_weights, mean=mean, verbose=verbose)
-                
+
                 # extrapolate state and output to update labels
                 _, sTr, oTr = zip(*[gnn.Loop(i) if i.problem_based != 'g' else super(GNNgraphBased, gnn).Loop(i) for i in gTr1])
-                gTr1 = [update_graph(i, s, o) for i, s, o in zip(gTr, sTr, oTr)]
+                gTr1 = [update_graph(i, s, gnn.output_activation(o)) for i, s, o in zip(gTr, sTr, oTr)]
                 if gVa:
                     _, sVa, oVa = zip(*[gnn.Loop(i) if i.problem_based != 'g' else super(GNNgraphBased, gnn).Loop(i) for i in gVa1])
-                    gVa1 = [update_graph(i, s, o) for i, s, o in zip(gVa, sVa, oVa)]
-        
-        else: super().train(gTr, epochs, gVa, update_freq, max_fails, class_weights, mean=mean, verbose=verbose)
+                    gVa1 = [update_graph(i, s, gnn.output_activation(o)) for i, s, o in zip(gVa, sVa, oVa)]
+
+        else:
+            super().train(gTr, epochs, gVa, update_freq, max_fails, class_weights, mean=mean, verbose=verbose)
