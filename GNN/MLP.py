384d9ea63f0aa96e92e67e617f95550f23c3934d
NickDrake117 <63723473+NickDrake117@users.noreply.github.com>
2021-05-12 14:45:03 +0200

    Add files via upload

--- GNN/MLP.py
+++ GNN/MLP.py
@@ -8,7 +8,7 @@ from tensorflow.keras.models import Sequential
 
 
 # ---------------------------------------------------------------------------------------------------------------------
-def MLP(input_dim: int, layers: list[int], activations, kernel_initializer, bias_initializer, kernel_regularizer = None,
+def MLP(input_dim: int, layers: list[int], activations, kernel_initializer, bias_initializer, kernel_regularizer=None,
         bias_regularizer=None, dropout_rate: Union[list[float], float, None] = None,
         dropout_pos: Optional[Union[list[int], int]] = None, alphadropout: bool = False):
     """ Quick building function for MLP model. All lists must have the same length
@@ -38,7 +38,7 @@ def MLP(input_dim: int, layers: list[int], activations, kernel_initializer, bias
     if type(dropout_rate) == float: dropout_rate = [dropout_rate for _ in dropout_pos]
 
     # check lengths
-    if len(set(map(len, [activations, kernel_initializer, bias_initializer, kernel_regularizer, bias_regularizer, layers])))>1:
+    if len(set(map(len, [activations, kernel_initializer, bias_initializer, kernel_regularizer, bias_regularizer, layers]))) > 1:
         raise ValueError('Dense parameters must have the same length to be correctly processed')
     if len(dropout_rate) != len(dropout_pos):
         raise ValueError('Dropout parameters must have the same length to be correctly processed')
@@ -63,7 +63,7 @@ def MLP(input_dim: int, layers: list[int], activations, kernel_initializer, bias
 
 
 # ---------------------------------------------------------------------------------------------------------------------
-def get_inout_dims(net_name: str, dim_node_label: int, dim_arc_label: int, dim_target: int, problem: str, dim_state: int,
+def get_inout_dims(net_name: str, dim_node_label: int, dim_arc_label: int, dim_target: int, problem_based: str, dim_state: int,
                    hidden_units: Union[None, int, list[int]],
                    *, layer: int = 0, get_state: bool = False, get_output: bool = False) -> tuple[int, list[int]]:
     """ Calculate input and output dimension for the MLP of state and output
@@ -72,7 +72,7 @@ def get_inout_dims(net_name: str, dim_node_label: int, dim_arc_label: int, dim_t
     :param dim_node_label: (int) dimension of node label
     :param dim_arc_label: (int) dimension of arc label
     :param dim_target: (int) dimension of target
-    :param problem: (str) s.t. len(problem) in [1,2] -> [{'a','n','g'} | {'1','2'}]
+    :param problem_based: (str) s.t. len(problem_based) in [1,2] -> [{'a','n','g'} | {'1','2'}]
     :param dim_state: (int)>=0 for state dimension paramenter of the gnn
     :param hidden_units: (int or list of int) for specifying units on hidden layers
     :param layer: (int) LGNN USE: get the dims at gnn of the layer <layer>, from graph dims on layer 0. Default is 0, since GNN==LGNN in this case
@@ -80,32 +80,31 @@ def get_inout_dims(net_name: str, dim_node_label: int, dim_arc_label: int, dim_t
     :param get_output: (bool) LGNN USE: set accordingly to LGNN behaviour, if gnns get state, output or both from previous layer
     :return: (tuple) (input_shape, layers) s.t. input_shape (int) is the input shape for mlp, layers (list of ints) defines hidden+output layers
     """
-    assert len(problem) in [1,2]
     assert layer >= 0
+    assert problem_based in ['a', 'n', 'g']
+    assert dim_state >= 0
 
-    if len(problem) == 1: problem += '1'
     DS = dim_state
     NL, AL, T = dim_node_label, dim_arc_label, dim_target
 
     # if LGNN, get MLPs layers for gnn in layer 2+
     if layer > 0:
-        GS, GO, P = get_state, get_output, problem[0]
+        GS, GO = get_state, get_output
         if DS != 0:
-            NL = NL + DS * GS + T * (P != 'a') * GO
-            AL = AL + T * (P == 'a') * GO
+            NL = NL + DS * GS + T * (problem_based != 'a') * GO
+            AL = AL + T * (problem_based == 'a') * GO
         else:
-            NL = NL + layer * NL * GS + ((layer - 1) * GS + 1) * T * (P != 'a') * GO
-            AL = AL + T * (P == 'a') * GO
+            NL = NL + layer * NL * GS + ((layer - 1) * GS + 1) * T * (problem_based != 'a') * GO
+            AL = AL + T * (problem_based == 'a') * GO
 
     # MLP state
     if net_name == 'state':
-        input_shape = AL + 2 * NL + DS * (1 + (problem[1] == '1'))
-        input_shape += NL * (DS == 0) * (problem[1] == '2')
+        input_shape = AL + 2 * (NL + DS)
         output_shape = DS if DS else NL
 
     # MLP output
     elif net_name == 'output':
-        input_shape = (problem[0] == 'a') * (NL + AL + DS) + NL + dim_state
+        input_shape = (problem_based == 'a') * (NL + AL + DS) + NL + dim_state
         output_shape = T
 
     # possible values for net_name in ['state','output'], otherwise raise error
